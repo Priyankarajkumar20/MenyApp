fc08d52202644164024f02adc99eed8d542c9c94
Priyankarajkumar20 <46425415+Priyankarajkumar20@users.noreply.github.com>
2019-05-16 18:18:28 +0530

    Update categories.component.js

--- src/Category/categories.component.js
+++ src/Category/categories.component.js
@@ -1,9 +1,6 @@
 (function () {
     angular.module('MenuApp')
-        .config(['$sceDelegateProvider', function ($sceDelegateProvider) {
-            $sceDelegateProvider.resourceUrlWhitelist(['**']);
-        }])
-        .component('categoryList', {      
+             .component('categoryList', {      
             templateUrl: 'src/Category/menu.template.html',
             bindings: {
                 items: '<',
